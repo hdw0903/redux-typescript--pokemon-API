66d225df7754c0b1b29dceca2d118bf9fc26cea3
Han Dongwon <hdw0903@naver.com>
2021-04-27 21:57:47 +0900

    first commit

---
+++ src/actions/PokemonActions.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { Dispatch } from "react";
+import { PokemonDispatchType, POKEMON_FAIL, POKEMON_SUCCESS } from "./PokemonActionTypes";
+
+
+export const fetchPokemonData = (pokemonName:string) => async (dispatch:Dispatch<PokemonDispatchType>) => {
+    try{
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+        const data = res.data
+        console.log(res)
+
+        dispatch({
+            type:POKEMON_SUCCESS,
+            payload:data
+        })
+    }catch (error) {
+        dispatch({
+            type:POKEMON_FAIL
+        })
+
+    }
+}
